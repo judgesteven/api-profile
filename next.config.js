67e4df17625c72b73a0347014426616011a75b3a
Steven Judge <stevenjudge@Stevens-MacBook-Pro.local>
2025-04-26 20:01:45 +0300

    chore: update Next.js config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
+  images: {
+    domains: ['images.gamelayer.co'],
+  },
 }
 
 module.exports = nextConfig 
