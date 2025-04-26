7d9e237d7c1aebd451eecff321b29bab386303aa
Steven Judge <stevenjudge@Stevens-MacBook-Pro.local>
2025-04-26 20:18:18 +0300

    fix: remove invalid experimental.appDir configuration

--- next.config.js
+++ next.config.js
@@ -8,14 +8,7 @@ const nextConfig = {
   },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false };
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': require('path').resolve(__dirname, 'src'),
-    };
     return config;
-  },
-  experimental: {
-    appDir: true,
   },
   async headers() {
     return [
